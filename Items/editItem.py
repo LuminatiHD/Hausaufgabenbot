c1dd51b50df17d1a5d4bb3845672d2a9b034ed24
LuminatiHD <yoan.mueller@protonmail.com>
2022-03-10 16:10:52 +0100

    edit modifies multiple aspects at once

--- Items/editItem.py
+++ Items/editItem.py
@@ -14,7 +14,8 @@ Itemkategorien = ("Test", "Aufgabe")
 
 async def editItem(self, ctx, selecteditem, editor):
     await editor.edit("Was genau möchtest du editieren?")
-    editorbtn = Buttons.EditButtons(ctx)
+
+    editorbtn = Buttons.Dropdown_Menu(ctx, [i.capitalize() for i in tablecategories], min=1, max=len(tablecategories))
     await editor.edit(view=editorbtn)
     await editorbtn.wait()
 
@@ -22,9 +23,9 @@ async def editItem(self, ctx, selecteditem, editor):
         i.disabled = True
 
     await editor.edit(view=editorbtn)
-
+    await editorbtn.wait()
 # ============================================== KATEGORIE ==============================================
-    if "kategorie" in editorbtn.edit:
+    if "Kategorie" in editorbtn.output:
         confirm = Buttons.Confirm(ctx)
         while not confirm.confirm:
             confirm = Buttons.Confirm(ctx)
@@ -49,13 +50,84 @@ async def editItem(self, ctx, selecteditem, editor):
                 (newcategory, selecteditem[5]))
             database.commit()
 
+# ============================================== ACCESS ==============================================
+    if "Access" in editorbtn.output:
+        confirm = Buttons.Confirm(ctx)
+        while not confirm.confirm:
+            newacc = Buttons.ManageItemAccess(ctx)
+            confirm = Buttons.Confirm(
+                ctx)  # ig tue dr button neu generiere wöu schüsch chasch dr button nümme drücke
+            # de funktioniert z confirme nid.
+            await editor.edit(content="Zugriff: ", view=newacc)
+            await newacc.wait()
+            oldaccess = selecteditem[4]
+            if selecteditem[4].isnumeric():
+                oldaccess = "private"
+            await editor.edit(f"Alter Zugriff: {oldaccess}\nNeuer Zugriff: "
+                              f"{newacc.access}.\nBestätigen?", view=confirm)
+            await confirm.wait()
+            access = newacc.access
+            if newacc.access == "private":
+                access = ctx.author.id
+        database.cursor().execute(
+            f"UPDATE {Itemtable} SET access = '{access}' WHERE rowid = ?", (selecteditem[5],))
+        database.commit()
+
+# ============================================== DATUM ==============================================
+    if "Datum" in editorbtn.output:
+        confirm = Buttons.Confirm(ctx)
+
+        while not confirm.confirm:
+
+            confirm = Buttons.Confirm(ctx)
+
+            menu = Buttons.ChooseDatum(ctx)
+            await editor.edit("Wann ist der Test oder die Aufgabe fällig?", view=menu)
+
+            while not menu.over:
+                menu = Buttons.ChooseDatum(ctx, menu.day, menu.month, menu.year)
+                await editor.edit(view=menu)
+                await menu.wait()
+                if menu.exit:
+                    break
+
+            if menu.exit:
+                break
+
+            for i in menu.children:
+                i.disabled = True
+
+            await editor.edit(view=menu)
+
+            datum = datetime.date(int(menu.year), int(menu.month), int(menu.day))
+
+            olddatum = datetime.date(int(selecteditem[0].split("-")[0]),
+
+                                     int(selecteditem[0].split("-")[1]),
+
+                                     int(selecteditem[0].split("-")[2]))
+
+            await editor.edit(content=f"Altes Datum: {olddatum.day}.{olddatum.month}.{olddatum.year}"
+
+                                         f"\nNeues Datum: {datum.day}.{datum.month}.{datum.year}\nBestätigen?",
+                                         view=confirm)
+
+            await confirm.wait()
+
+        if not menu.exit:
+            database.cursor().execute(f"UPDATE {Itemtable} SET datum = ? WHERE rowid = ?",
+                                      (str(datum), selecteditem[5]))
+            database.commit()
+
 # ============================================== AUFGABE ==============================================
-    elif "aufgabe" in editorbtn.edit:
+    if "Aufgabe" in editorbtn.output:
         confirm = Buttons.Confirm(ctx)
         while not confirm.confirm:
             confirm = Buttons.Confirm(ctx)
-            await ctx.reply("Aufgabe: ")
+            askmsg = await ctx.reply("Aufgabe: ")
             newaufg = await self.bot.wait_for("message", check=lambda msg: msg.author == ctx.author)
+
+            await askmsg.delete()
             confirmmsg = await newaufg.reply(f"Alte Aufgabe: {selecteditem[3]}\nNeue Aufgabe: {newaufg.content}\nBestätigen?",
                                 view=confirm)
             await confirm.wait()
@@ -112,13 +184,14 @@ async def editItem(self, ctx, selecteditem, editor):
             database.commit()
 
 # ============================================== FACH ==============================================
-    elif "fach" in editorbtn.edit:
+    if "Fach" in editorbtn.output:
         confirm = Buttons.Confirm(ctx)
         while not confirm.confirm:
             confirm = Buttons.Confirm(ctx)
-            await ctx.reply("Fach: ")
+            askmsg = await ctx.reply("Fach: ")
             newfach = await self.bot.wait_for("message", check=lambda msg: msg.author == ctx.author)
 
+            await askmsg.delete()
             confirmmsg = await newfach.reply(f"Altes Fach: {selecteditem[2]}\nNeues Fach: "
                                 f"{FuncLibrary.changefachname(newfach.content)}.\nBestätigen?", view=confirm)
             await confirm.wait()
